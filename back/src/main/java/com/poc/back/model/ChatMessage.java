7a6c1a527326895a0799d67bcd69b13dc8b0e58c
Arthur LEVESQUE <98586610+Arthurl35@users.noreply.github.com>
2024-09-07 10:52:00 +0200

    Ajout du modèle ChatMessage avec types de message CHAT, JOIN et LEAVE

--- back/src/main/java/com/poc/back/model/ChatMessage.java
+++ back/src/main/java/com/poc/back/model/ChatMessage.java
@@ -1,1 +1,13 @@
 package com.poc.back.model;
+
+public class ChatMessage {
+    private String content;
+    private String sender;
+    private MessageType type;
+
+    public enum MessageType {
+        CHAT,
+        JOIN,
+        LEAVE
+    }    
+}
