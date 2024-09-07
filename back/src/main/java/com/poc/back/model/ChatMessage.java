761028251bd41ee549604431fa05fcaca7a1e732
Arthur LEVESQUE <98586610+Arthurl35@users.noreply.github.com>
2024-09-07 11:08:24 +0200

    Ajout des annotations Lombok

--- back/src/main/java/com/poc/back/model/ChatMessage.java
+++ back/src/main/java/com/poc/back/model/ChatMessage.java
@@ -1,5 +1,12 @@
 package com.poc.back.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class ChatMessage {
     private String content;
     private String sender;
